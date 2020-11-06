5f084c0310dd3e8a856a2d6d1baa0faa8fcfc60d
Azeez Eniseyin <heniiseyhin@gmail.com>
2020-11-06 16:39:15 +0100

    System Architecture

--- job/partials/promo.php
+++
@@ -1,7 +0,0 @@
-<div class="well">
-    <p>Take advantage of our <span class="text"><a href="#"> PROMO PRICE</a></span> of <span class="text">N39, 990 </span> instead of <span class="text">N99, 990 </span>and get over <span class="text">50% OFF?  </span> This is a great
-        offer and will only run for a limited period of time so hurry.
-    </p>
-    <a href="register.php" class="btn secondary-btn text-center">Enroll Now!!!!<span class="flaticon-next"></span></a>
-    <!--p class="text">Enroll Now!!!! </p-->
-</div>
